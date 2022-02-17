d82f9b45cd147e7fb76745494c7d3f68bb5c4f5f
Simon Wong <Simon@Mac-7.local>
2022-02-18 09:04:23 +1100

    Testing pygames

---
+++ sudoku/sudokuGraphics.py
@@ -0,0 +1,13 @@
+import pygame
+
+pygame.init()
+
+display_screen = pygame.display.set_mode((600,600))
+
+pygame.display.set_caption("pygame test")
+
+pygame.display.update()
+
+pygame.quit()
+
+quit()
