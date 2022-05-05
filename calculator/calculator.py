864ad7f27036d2aa313ab5e4bbceaf26265f6abd
Simon <simon.kj.wong@gmail.com>
2022-05-05 11:10:07 +1000

    Slight reconfigure

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -4,50 +4,46 @@ from tkinter import ttk
 
 def create_calculator_frame(container):
 
-    # Button functions
-    def button_clear():
-        display = None
-
-    def number_press(num):
-        display = str(num)
-        ttk.Label(frame, text = str(display)).grid(column=0, row=0, columnspan=4, rowspan=2, padx=10, pady=10)
-
-    def decimal_press(display):
-        if '.' not in display:
-            display += '.'
-        ttk.Label(frame, text = str(display)).grid(column=0, row=0, columnspan=4, rowspan=2, padx=10, pady=10)
-
+    # Create the tkinter window
     frame = ttk.Frame(container)
 
-    # Functions contains both buttons and display to account for direct interactions between the two
-    # Display only holds what should be labelled with computations running in background
-    display = None
-    if display is None:
-        ttk.Label(frame, text = '0').grid(column=0, row=0, columnspan=4, rowspan=2, padx=10, pady=10)
-    else:
-        ttk.Label(frame, text = str(display)).grid(column=0, row=0, columnspan=4, rowspan=2, padx=10, pady=10)
+    # Initially equation is empty
+    equation = ""
+
+    # Function to update the equation widget content
+    def number_press(num, equation):
+        # Uses the global variable "equation"
+        equation += str(num)
+
+        # Configure the label
+        equation_label.config(text = equation)
+
 
     # Numbers
-    ttk.Button(frame, text='0', command=lambda num=0: number_press(num)).grid(column=0, row=6)
-    ttk.Button(frame, text='1', command=lambda num=1: number_press(num)).grid(column=0, row=5)
-    ttk.Button(frame, text='2', command=lambda num=2: number_press(num)).grid(column=1, row=5)
-    ttk.Button(frame, text='3', command=lambda num=3: number_press(num)).grid(column=2, row=5)
-    ttk.Button(frame, text='4', command=lambda num=4: number_press(num)).grid(column=0, row=4)
-    ttk.Button(frame, text='5', command=lambda num=5: number_press(num)).grid(column=1, row=4)
-    ttk.Button(frame, text='6', command=lambda num=6: number_press(num)).grid(column=2, row=4)
-    ttk.Button(frame, text='7', command=lambda num=7: number_press(num)).grid(column=0, row=3)
-    ttk.Button(frame, text='8', command=lambda num=8: number_press(num)).grid(column=1, row=3)
-    ttk.Button(frame, text='9', command=lambda num=9: number_press(num)).grid(column=2, row=3)
+    ttk.Button(frame, text='0', command=lambda num=0, equation = equation: number_press(num, equation)).grid(column=0, row=6)
+    ttk.Button(frame, text='1', command=lambda num=1, equation = equation: number_press(num, equation)).grid(column=0, row=5)
+    ttk.Button(frame, text='2', command=lambda num=2, equation = equation: number_press(num, equation)).grid(column=1, row=5)
+    ttk.Button(frame, text='3', command=lambda num=3, equation = equation: number_press(num, equation)).grid(column=2, row=5)
+    ttk.Button(frame, text='4', command=lambda num=4, equation = equation: number_press(num, equation)).grid(column=0, row=4)
+    ttk.Button(frame, text='5', command=lambda num=5, equation = equation: number_press(num, equation)).grid(column=1, row=4)
+    ttk.Button(frame, text='6', command=lambda num=6, equation = equation: number_press(num, equation)).grid(column=2, row=4)
+    ttk.Button(frame, text='7', command=lambda num=7, equation = equation: number_press(num, equation)).grid(column=0, row=3)
+    ttk.Button(frame, text='8', command=lambda num=8, equation = equation: number_press(num, equation)).grid(column=1, row=3)
+    ttk.Button(frame, text='9', command=lambda num=9, equation = equation: number_press(num, equation)).grid(column=2, row=3)
     ttk.Button(frame, text='.').grid(column=1, row=6)
 
     # Functional Buttons
     ttk.Button(frame, text='Quit', command=container.destroy).grid(column=2, row=6)
-    ttk.Button(frame, text='Clear', command=button_clear()).grid(column=0, row=2)
+    ttk.Button(frame, text='Clear').grid(column=0, row=2)
     ttk.Button(frame,text='=').grid(column=3, row=6)
     ttk.Button(frame,text='+').grid(column=3, row=5)
     ttk.Button(frame,text='-').grid(column=3, row=4)
     ttk.Button(frame,text='*').grid(column=3, row=3)
     ttk.Button(frame,text='÷').grid(column=3, row=2)
+
+    # Create the equation display widget
+    equation_label = ttk.Label(frame, text = equation)
+    equation_label.grid(column=0, row=0, columnspan=4, rowspan=2, padx=10, pady=10)
 
     return frame
 
